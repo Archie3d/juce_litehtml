9bce01e57e361d92c6eda76e3499c74c4fe0cd11
Arthur Benilov <arthur.benilov@gmail.com>
2022-05-05 21:20:35 +0100

    Initial commit.

---
+++ juce_litehtml/juce_litehtml.cpp
@@ -0,0 +1,5 @@
+#ifdef JUCE_LITEHTML_H_INCLUDED
+#   error "Incorrect use of cpp file"
+#endif
+
+#include "juce_litehtml.h"
---
+++ juce_litehtml/juce_litehtml.h
@@ -0,0 +1,27 @@
+/*
+ ==============================================================================
+
+    BEGIN_JUCE_MODULE_DECLARATION
+
+    ID:            juce_litehtml
+    vendor:        Arthur Benilov
+    version:       1.0.0
+    name:          litehtml
+    description:   HTML rendering engine
+    website:       https://github.com/Archie3d/juce_litehtml
+    license:       GPL/BSD-3
+
+    dependencies:  juce_gui_basics, juce_gui_extra
+
+    END_JUCE_MODULE_DECLARATION
+
+ ==============================================================================
+*/
+
+#pragma once
+#define JUCE_LITEHTML_H_INCLUDED
+
+#include <juce_core/juce_core.h>
+#include <juce_data_structures/juce_data_structures.h>
+#include <juce_gui_basics/juce_gui_basics.h>
+
