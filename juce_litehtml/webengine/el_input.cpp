c4c7b2a92f4ee2d8ee8578dd07d40ee1395794da
Arthur Benilov <arthur.benilov@gmail.com>
2022-05-14 18:57:11 +0100

    Input colours.

--- juce_litehtml/webengine/el_input.cpp
+++ juce_litehtml/webengine/el_input.cpp
@@ -7,28 +7,37 @@ el_input::el_input (const litehtml::document::ptr& doc)
 
 void el_input::parse_attributes()
 {
-    DBG("<input>");
-    for (auto&& [key, value] : m_attrs)
-    {
-        DBG("    '" << String(key) << "' = '" << String(value) << "'");
-    }
-
     const String type { get_attr("type") };
 
-    if (type == "text" || type == "search")
+    const auto colour = webColour (get_color ("color", true, get_document()->get_def_color()));
+    auto bgColour = webColour (get_color ("background-color", true, litehtml::web_color(255, 255, 255, 255)));
+
+    if (auto* bg { get_background(true) })
+        bgColour = webColour (bg->m_color);
+
+    if (type.isEmpty() || type == "text" || type == "search")
     {
         textEditor = std::make_unique<TextEditor>();
-        textEditor->setText(get_attr("value"));
+        textEditor->setText (get_attr("value"));
+
+        textEditor->setColour (TextEditor::textColourId, colour);
+        textEditor->setColour (TextEditor::backgroundColourId, bgColour);
+
         component = textEditor.get();
     }
     else if (type == "submit")
     {
-        //textButton = std::make_unique<TextButton>(get_attr("value", "Submit"));
-        //component = textButton.get();
+        textButton = std::make_unique<TextButton>(get_attr("value", "Submit"));
+
+        textButton->setColour (TextButton::textColourOnId, colour);
+        textButton->setColour (TextButton::buttonColourId, bgColour);
+
+        component = textButton.get();
     }
     else if (type == "checkbox")
     {
         toggleButton = std::make_unique<ToggleButton>(get_attr("value"));
+
         component = toggleButton.get();
     }
 
